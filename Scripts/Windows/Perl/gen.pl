e440a7b0b385a5a4aaddf6b011871aaa81c8c51a
Sebastian Termen <sebastian.termen@gmail.com>
2023-10-04 21:20:18 +0300

    Further improvements to CMakeLists.txt presets.

--- Scripts/Windows/Perl/gen.pl
+++ Scripts/Windows/Perl/gen.pl
@@ -14,11 +14,58 @@ my $this_dir;
 my $provided_dir;
 
 # Variables to set up CMake project
+# LIST OF MODIFIABLE VARIABLES
+# |CMAKE_MINIMUM_VERSION| - Minimum version of CMake required | 3.22 |
+# |CXX_OR_C_STANDARD| - Standard of C++ or C | C or CXX |
+# |CXX_OR_C_VERSION_NAME| - Version of C++ or C | 99, 11, 14, 17, 20 |
+# |TESTING| - Whether or not to include testing | ON or OFF|
+# |EXAMPLES| - Whether or not to include examples | ON or OFF|
+# |PROJECT_NAME| - Name of project | MyProject |
+# |BUILD_PY_LOCATION| - Location of build.py file | PATH TO OS SPECIFIC BUILD.PY |
+# |IF_VCPKG| - Whether or not to use vcpkg | ON or OFF |
+# |VCPKG_LIBS| - List of vcpkg dependencies | "find_package(GiT QUIET)\n", "find_package(fmt CONFIG REQUIRED)\n", etc... |
+# |VCPKG_LIBS_TARGETS| - List of vcpkg dependencies to provide target links
+# "fmt::fmt\n"
+# "spdlog::spdlog\n"
+# "OpenGL::GL\n"
+# "${OPENGL_LIBRARIES}\n"
+# "${raylib_LIBRARIES}\n"
+# "${Python3_LIBRARIES}\n"
+# "${Boost_LIBRARIES}\n"
+# "${VCPKG_CORE}/lib/${PYTHON_VERSION}.lib\n"
+# "PkgConfig::GTKMM_LIB\n"
+# etc...
+# |
+# |VCPOKG_INCLUDE_TARGETS| - List of vcpkg dependencies to provide target includes
+# |
+# "${PROJECT_SOURCE_DIR}/src\n"
+# "${OPENGL_INCLUDE_DIR}\n"
+# "${raylib_INCLUDE_DIRS}\n"
+# "${Python3_INCLUDE_DIRS}\n"
+# "${Boost_INCLUDE_DIRS}\n"
+# "${GTKMM_LIB_INCLUDE_DIRS}\n"
+# |
+# |IF_PYTHON| - Whether or not to use Python vcpkg library
+# |IF_BOOST| - Whether or not to use Boost vcpkg library
+# |IF_STB| - Whether or not to use stb vcpkg library
+# |PKG_CONFIG_LIBS| - List of pkg-config dependencies
+# |
+# pkg_check_modules(GTKMM_LIB REQUIRED IMPORTED_TARGET gtkmm-4.0)
+#        if(GTKMM_LIB_FOUND)
+#                message(STATUS "GTKmm found")
+#        else()
+#                message(FATAL_ERROR "GTKmm not found")
+#        endif()
+# |
+# |EXE_NAME| - Name of executable | HelloWorld |
+# |IF_LIBRARY| - Whether or not to create a library | ON or OFF |
+# |LIB_NAME| - Name of library | MyLibrary |
+# LIST OF MODIFIABLE VARIABLES
+
 my $project_name           = 'MyProject';
 my $cmake_minimum_required = '3.22';
 my $language               = 'CXX';
 my $cxx_standard           = '11';
-my $x64flag                = 1;
 
 my $testing  = 0;
 my $examples = 0;
@@ -28,6 +75,8 @@ my $exe_name = 'HelloWorld';
 
 my $vcpkg      = 0;
 my @vcpkg_libs = ();
+
+my $build_py_location = "";
 
 my $current_menu = 'main_menu';
 
